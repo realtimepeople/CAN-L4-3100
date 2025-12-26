4f75c1f6b8f3ca03726e872ada91db577d307645
realtimepeople <klaus.schaefer@h-da.de>
2025-12-26 18:47:28 +0100

    Output format changed to 3*14 bits -> 6 bytes.

--- CAN-L4_3100/Magnetometer/RM3100.cpp
+++ CAN-L4_3100/Magnetometer/RM3100.cpp
@@ -130,7 +130,7 @@ extern "C" void RM3100_runnable( void *)
 	uint64_t packed_result;
 
 	volatile HAL_StatusTypeDef stat = HAL_CAN_Start(&hcan1);
-	CAN_TxHeaderTypeDef Header = { 0x160, 0, 0, 0, 8, DISABLE};
+	CAN_TxHeaderTypeDef Header = { 0x160, 0, 0, 0, 6, DISABLE};
 	uint32_t mbx;
 
 	while( true)
@@ -165,12 +165,12 @@ extern "C" void RM3100_runnable( void *)
 	    if( not  result)
 	    	continue;
 
-	    measurement_result.magx = ((target.magx_2 << 24) | (target.magx_1 << 16) | (target.magx_0 << 8)) & 0x1fffff00;
-		measurement_result.magy = ((target.magy_2 << 24) | (target.magy_1 << 16) | (target.magy_0 << 8)) & 0x1fffff00;
-		measurement_result.magz = ((target.magz_2 << 24) | (target.magz_1 << 16) | (target.magz_0 << 8)) & 0x1fffff00;
+	    measurement_result.magx = ((target.magx_2 << 24) | (target.magx_1 << 16) | (target.magx_0 << 8)) & 0x3fff00;
+		measurement_result.magy = ((target.magy_2 << 24) | (target.magy_1 << 16) | (target.magy_0 << 8)) & 0x3fff00;
+		measurement_result.magz = ((target.magz_2 << 24) | (target.magz_1 << 16) | (target.magz_0 << 8)) & 0x3fff00;
 
 		// pack result into single 64 bit datum: 21 + 21 + 21 bits
-		packed_result = (measurement_result.magx >> 8) | ((uint64_t)(measurement_result.magy) << (21-8)) | ((uint64_t)(measurement_result.magz) << (2*21-8));
+		packed_result = (measurement_result.magx >> 8) | ((uint64_t)(measurement_result.magy) << (14-8)) | ((uint64_t)(measurement_result.magz) << (2*14-8));
 	    result = HAL_CAN_AddTxMessage( &hcan1, &Header, (uint8_t *)&packed_result, &mbx);
 	}
 }
